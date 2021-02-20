6ffcd1de326632a878ceb86d4cd8ddd0a9667192
mike <mike0x73@gmail.com>
2021-02-20 10:47:21 -0500

    setup polybar

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ cd /opt
 git clone https://github.com/maestrogerardo/i3-gaps-deb.git
 cd i3-gaps-deb 
 ./i3-gaps-deb
-apt install compton ranger polybar
+apt install compton ranger polybar rofi calc
 mkdir ~/.config/i3
 cd $start_dir
-cp ./i3/config ~/.config/i3/config
+cp -r ./i3 ~/.config
+cp -r ./rofi ~/.config
+cp -r ./polybar ~/.config
 echo "=== Now restart to run i3 ==="
