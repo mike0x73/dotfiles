dc6191fcb32470873eb0647fdf0332b4347290f3
mike <mike0x73@gmail.com>
2021-03-28 15:19:28 +0100

    added zshrc file

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ cp -r i3 /home/kali/.config
 cp -r rofi /home/kali/.config
 cp -r polybar /home/kali/.config
 cp -r kitty /home/kali/.config
+cp .zshrc /home/kali/.zshrc -f
 chown kali:kali /home/kali/.config -R
+chown kali:kali /home/kali/.zshrc
 timedatectl set-timezone Europe/London
 echo "=== Now restart to run i3 ==="
--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ cp -r i3 /home/kali/.config
 cp -r rofi /home/kali/.config
 cp -r polybar /home/kali/.config
 cp -r kitty /home/kali/.config
+cp .zshrc /home/kali/.zshrc -f
 chown kali:kali /home/kali/.config -R
+chown kali:kali /home/kali/.zshrc
 echo Done
