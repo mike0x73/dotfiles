d934cea893724d162b127a82cd84b2e17e4b3366
mike <mike0x73@gmail.com>
2021-04-10 19:06:10 +0100

    added xfce login screen dark theme

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ cp -r rofi /home/kali/.config
 cp -r polybar /home/kali/.config
 cp -r kitty /home/kali/.config
 cp .zshrc /home/kali/.zshrc -f
+sudo cp -f lightdm/lightdm-gtk-greeter.conf /etc/lightdm
 chown kali:kali /home/kali/.config -R
 chown kali:kali /home/kali/.zshrc
 echo Done
