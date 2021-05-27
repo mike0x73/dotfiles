ce2da668ff85b5b79938149e6caec88fa44f4ea5
mike0x73 <mike0x73@gmail.com>
2021-05-27 21:33:51 +0100

    moved hacking folder

--- docker/kali/make-kali.sh
+++ docker/kali/make-kali.sh
@@ -4,4 +4,4 @@ echo 'alias kali-start="docker start -ai kali"' >> $HOME/.zshrc
 echo 'alias kali="docker exec -it kali /bin/sh -c \"bash\""' >> $HOME/.zshrc
 
 docker build . -t kali
-docker run --name kali -v $HOME/hacking:/root/hacking -it kali:latest "/bin/bash"
+docker run --name kali -v $HOME/hacking:/hacking -it kali:latest "/bin/bash"
