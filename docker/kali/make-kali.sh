31df012ef8924b1e10fd288b5edba6b46d8bb642
mike0x73 <mike0x73@gmail.com>
2021-05-28 16:42:42 +0100

    improved kali docker

--- docker/kali/make-kali.sh
+++ docker/kali/make-kali.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo 'alias kali-start="docker start -ai kali"' >> $HOME/.zshrc
-echo 'alias kali="docker exec -it kali /bin/sh -c \"bash\""' >> $HOME/.zshrc
+if ! grep -q "alias kali-start" $HOME/.zshrc;
+	then echo 'alias kali-start="docker start -ai kali"' >> $HOME/.zshrc && echo 'alias kali="docker exec -it kali /bin/sh -c \"bash\""' >> $HOME/.zshrc;
+fi
 
 docker build . -t kali
 docker run --name kali -v $HOME/hacking:/hacking -it kali:latest "/bin/bash"
