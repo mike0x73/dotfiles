8b93402fe65a16fca8b728cea281a0ee3651e900
mike0x73 <mike0x73@gmail.com>
2021-05-29 16:09:20 +0100

    updated wallpapers

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ cp -r i3 /home/kali/.config
 cp -r rofi /home/kali/.config
 cp -r polybar /home/kali/.config
 cp -r kitty /home/kali/.config
+cp -r Wallpapers /home/kali/Pictures
 cp .zshrc /home/kali/.zshrc -f
 chown kali:kali /home/kali/.config -R
 chown kali:kali /home/kali/.zshrc
--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ cp -r i3 /home/kali/.config
 cp -r rofi /home/kali/.config
 cp -r polybar /home/kali/.config
 cp -r kitty /home/kali/.config
+cp -r Wallpapers /home/kali/Pictures
 cp .zshrc /home/kali/.zshrc -f
 sudo cp -f lightdm/lightdm-gtk-greeter.conf /etc/lightdm
 chown kali:kali /home/kali/.config -R
