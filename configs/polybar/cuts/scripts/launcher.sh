109c1c51aa84626de13aeb6685a37633d2a7a9fb
mike0x73 <mike0x73@gmail.com>
2023-03-11 19:42:52 +0000

    new dotfiles

---
+++ configs/polybar/cuts/scripts/launcher.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/home/mike/.config/rofi/launchers/type-1/launcher.sh
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+bash apt_sources.txt
+sudo apt update -y
+sudo apt upgrade -y
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  sudo apt install -y "$p"
+done < apt_packages.txt
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  sudo pip install "$p"
+done < pip_installs.txt
+
+bash custom_installs.txt
+
+cp -r configs/* ~/.config
+mkdir ~/shares
+sudo cp -f backgrounds/* /usr/share/backgrounds
+echo "Reboot to enable i3"
